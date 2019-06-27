f949ad5aa5cfa8dbf969a9b00bfbf5f1b90be03a
chenhu0818 <br_3615381290@126.com>
2019-06-27 22:39:44 +0800

    2 save

--- day_6_1.py
+++ day_6_1.py
@@ -2,13 +2,31 @@ import re
 
 asa_conn = "TCP Student 192.168.189.167:32806 Teacher 137.78.5.128:65247, idle 0:00:00, bytes 74, flags UIO\n TCP Student 192.168.189.167:80 Teacher 137.78.5.128:65233, idle 0:00:03, bytes 334516, flags UIO"
 
-asa_infoamation = asa_conn.split('\n')
-asa_infoamation_line1 = asa_infoamation[0]
-asa_infoamation_line2 = asa_infoamation[1]
-asa_infoamation_key = re.findall('(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5}).*\s(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5})',asa_infoamation_line1)
-asa_infoamation_key2 = re.findall('(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5}).*\s(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5})',asa_infoamation_line2)
-asa_infoamation_value = re.findall('bytes\s(\d{1,10}).*\s+(UIO)?',asa_infoamation_line1)
-asa_infoamation_value2 = re.findall('bytes\s(\d{1,10}).*\s+(UIO)?',asa_infoamation_line2)
+asa_dict = {}
 
-print(asa_infoamation_value)
+for x in asa_conn.split('\n'):
+    re_result = re.findall('(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5}).*\s(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5})',asa_conn)
+    re_result1 = re.findall('bytes\s(\d{1,10}).*\s+(UIO)?',asa_conn)
 
+asa_dict = {a:b for (a,b) in zip(re_result,re_result1)}
+
+print(asa_dict)
+
+src = 'src'
+src_ip = 'src_ip'
+dst = 'dst'
+dst_ip = 'dst_ip'
+bytes_name = 'bytes'
+flags = 'flags'
+format_str1 =
+format_str2 =
+
+
+
+
+
+
+# asa_infoamation_key = re.findall('(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5}).*\s(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5})',asa_infoamation)
+# asa_infoamation_key2 = re.findall('(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5}).*\s(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5})',asa_infoamation)
+# asa_infoamation_value = re.findall('bytes\s(\d{1,10}).*\s+(UIO)?',asa_infoamation)
+# asa_infoamation_value2 = re.findall('bytes\s(\d{1,10}).*\s+(UIO)?',asa_infoamation)
