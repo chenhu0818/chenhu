01d696149feae8ec66afdc4ab2e6f661b147395b
chenhu0818 <br_3615381290@126.com>
2019-06-28 20:18:30 +0800

    2 save

--- day_6_1.py
+++ day_6_1.py
@@ -5,12 +5,11 @@ asa_conn = "TCP Student 192.168.189.167:32806 Teacher 137.78.5.128:65247, idle 0
 asa_dict = {}
 
 for x in asa_conn.split('\n'):
-    re_result = re.findall('(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5}).*\s(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5})',asa_conn)
-    re_result1 = re.findall('bytes\s(\d{1,10}).*\s+(UIO)?',asa_conn)
+    re_result = re.findall('\s(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}):(\d{2,5}).*\s(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}):(\d{2,5})',asa_conn)
+    re_result1 = re.findall('bytes\s(\d+).*\w+\s+(UIO){1,2}',asa_conn)
+
 
 asa_dict = {a:b for (a,b) in zip(re_result,re_result1)}
-
-print(asa_dict)
 
 src = 'src'
 src_ip = 'src_ip'
@@ -18,8 +17,32 @@ dst = 'dst'
 dst_ip = 'dst_ip'
 bytes_name = 'bytes'
 flags = 'flags'
-format_str1 =
-format_str2 =
+
+format_str = '{0:^10}:{1:^20}|{2:^10}:{3:^20} | {4:^10}:{5:^20} | {6:^10}:{7:^20}'
+format_str1 = '{0:^10}:{1:^20}|{2:^10}:{3:^20}'
+
+print('格式化打印输出\n')
+
+for key,value in asa_dict.items():
+    print(format_str.format(src,key[0],src_ip,key[1],dst,key[2],dst_ip,key[3]))
+    print(format_str1.format(bytes_name, value[0], flags, value[1]))
+    print('=' * 200)
+
+
+
+
+
+
+
+# print('打印分析后的字典\n')
+# print(asa_dict)
+# print('-'*100)
+#
+# for key,value in asa_dict.items():
+#     print(key,'\n',value,'\n','='*100)
+#
+
+
 
 
 
