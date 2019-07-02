45339e2e1f4e72b839439f126f01039f40e5af16
chenhu0818 <br_3615381290@126.com>
2019-07-02 14:59:18 +0800

    2 save

--- test.py
+++ test.py
@@ -1,17 +1,15 @@
-import re
+def qytang_ping(ip):
+    ping_pkt = IP(dst=ip) / ICMP()
+    ping_result = sr1(ping_pkt, timeout=2, verbose=False)
+    if ping_result:
+        return ip,1
+    else:
+        return ip,0
 
-asa_conn = "TCP Student 192.168.189.167:32806 Teacher 137.78.5.128:65247, idle 0:00:00, bytes 74, flags UIO\n TCP Student 192.168.189.167:80 Teacher 137.78.5.128:65233, idle 0:00:03, bytes 334516, flags UIO"
 
-asa_infoamation = asa_conn.split('\n')
-asa_infoamation_line1 = asa_infoamation[0]
-asa_infoamation_line2 = asa_infoamation[1]
-asa_infoamation_key = re.findall('(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5}).*\s(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5})',asa_infoamation_line1)
-asa_infoamation_key2 = re.findall('(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5}).*\s(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).?(\d{2,5})',asa_infoamation_line2)
-asa_infoamation_value = re.findall('bytes\s(\d{1,10}).*\s+(UIO)?',asa_infoamation_line1)
-asa_infoamation_value2 = re.findall('bytes\s(\d{1,10}).*\s+(UIO)?',asa_infoamation_line2)
-
-asa_list = [[asa_infoamation_key,asa_infoamation_value],[asa_infoamation_key2,asa_infoamation_value2]]
-
-asa_dict = {}
-for x in asa_list:
-    asa_dict[x[0]] = x[1]
+if __name__ == '__main__':
+    result = qytang_ping('137.78.5.254')
+    if result[1]:
+        print(result[0], '通!')
+    else:
+        print(result[0], '不通!')
